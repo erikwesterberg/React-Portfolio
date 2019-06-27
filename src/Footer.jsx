81f4dfd6c4234a7f93bb7a19092e886733128d23
erka <erik.westerberg@hotmail.se>
2019-06-27 12:58:21 +0200

    fixed the background image , boom

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="footer-wrapper">
                 <div className="footer-content">
-                Made with React | ERIK&nbsp;WESTEBERG 2019 |&nbsp;<a href="https://se.linkedin.com/in/erik-westerberg-24b06889" className="link" target="_blank" >LinkedIn</a>&nbsp;|&nbsp;<a href="https://github.com/erikwesterberg" className="link" target="_blank">GitHub</a>
+                Made with React | &nbsp;<a href="https://se.linkedin.com/in/erik-westerberg-24b06889" className="link" target="_blank" >LinkedIn</a>&nbsp;|&nbsp;<a href="https://github.com/erikwesterberg" className="link" target="_blank">GitHub</a>
                 </div>
         </div>
 )
--- src/contact.jsx
+++ src/contact.jsx
@@ -18,6 +18,7 @@ const Contact = () => {
         </div>
         </div>
         </div>
+        
     
             
             
