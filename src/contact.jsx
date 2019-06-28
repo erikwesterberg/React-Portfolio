db0dc8dba73f2d16dcdfd1f26d3691b487123e55
erka <erik.westerberg@hotmail.se>
2019-06-28 21:45:35 +0200

    added a new contact form from netlifly, looks like shit but easy to install

--- src/contact.jsx
+++ src/contact.jsx
@@ -5,20 +5,25 @@ import cool from "./image/hacker.png"
 
 const Contact = () => {
     return (
-        <div className="content-wrapper">
-            <div className="content-header">
-            <div className="image-header-wrapper">
-            <UndrawNewsletter primaryColor='#20504f' skinColor="#f3d3cd" hairColor="#b29a5e" height='100px'/>
-            </div>
-        
-            <div className="header-wrapper">
-            <h1 className="content-h1">Contact</h1>
-            <br></br>
-            <p className="content-text">If you want to contact me, as you should. Send a mail to: erik.westerberg@hotmail.se.</p>
-        </div>
-        </div>
-        </div>
-        
+        <form action="POST" data-netlify="true">
+								<div class="fields">
+									<div class="field half">
+										<input type="text" name="name" id="name" placeholder="Name" />
+									</div>
+									<div class="field half">
+										<input type="email" name="email" id="email" placeholder="Email" />
+									</div>
+									<div class="field">
+										<textarea name="message" id="message" placeholder="Message" rows="7"></textarea>
+									</div>
+									<div class="field">
+										<div data-netlify-recaptcha="true"></div>
+									</div>
+								</div>
+								<ul class="actions">
+									<li><input type="submit" value="Send Message" class="button primary" /></li>
+								</ul>
+							</form>
     
             
             
